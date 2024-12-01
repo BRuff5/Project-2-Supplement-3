e17711bfe8fbcfc12a98aae4efdf0d256e231687
BLR <bruff@Macky-Max.local>
2024-12-01 00:03:10 -0500

    last commit

--- main.java
+++ main.java
@@ -58,5 +58,72 @@ public class main {
         }
         return minor;
     }
+
+
+    /**
+     * Multiply two matrices.
+     *
+     * @param matrixA The first matrix.
+     * @param matrixB The second matrix.
+     * @return The product of the matrices.
+     * @throws IllegalArgumentException If the matrices cannot be multiplied.
+     */
+    public static int[][] multiplyMatrices(int[][] matrixA, int[][] matrixB) {
+        int rowsA = matrixA.length, colsA = matrixA[0].length;
+        int rowsB = matrixB.length, colsB = matrixB[0].length;
+        if (colsA != rowsB) {
+            throw new IllegalArgumentException("Matrix dimensions do not match for multiplication.");
+        }
+
+        int[][] product = new int[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                product[i][j] = 0;
+                for (int k = 0; k < colsA; k++) {
+                    product[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        return product;
+    }
+
+    // Test methods
+    public static void main(String[] args) {
+        // Binary Search Test
+        int[] sortedArray = {1, 3, 5, 7, 9};
+        System.out.println("Binary Search Test:");
+        System.out.println("Index of 5: " + binarySearch(sortedArray, 5)); // Should print 2
+        System.out.println("Index of 8: " + binarySearch(sortedArray, 8)); // Should print -1
+
+        // Determinant Test
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("\nDeterminant Test:");
+        System.out.println("Determinant: " + determinant(matrix)); // Should print 0
+
+        // Matrix Multiplication Test
+        int[][] matrixA = {
+            {1, 2},
+            {3, 4}
+        };
+        int[][] matrixB = {
+            {5, 6},
+            {7, 8}
+        };
+        System.out.println("\nMatrix Multiplication Test:");
+        int[][] result = multiplyMatrices(matrixA, matrixB);
+        for (int[] row : result) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+        // Should print:
+        // 19 22
+        // 43 50
+    }
 }
 
